9b08c2c76882c3f91325c172780c1633c31f1609
dethMastery <me@suphakit.net>
2023-04-28 11:51:22 +0700

    feat: modal

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,40 @@
 import "./App.css";
+import React from "react";
+import Modal from "react-modal";
 
 function App() {
+  const [modalIsOpen, setIsOpen] = React.useState(false);
+
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  const modalCard = 'absolute translate-x-[-50%] translate-y-[-50%] right-[50%] left-[50%] top-[20%] w-[80%] h-[30%] bg-[#2e2f2f] text-[whitesmoke] rounded-[1rem] p-4 turnOn'
+
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Vite + React + TS + Tailwind.css</h1>
+      <div>
+        <button onClick={openModal}>Open Modal</button>
+        <Modal
+          isOpen={modalIsOpen}
+          onAfterOpen={afterOpenModal}
+          onRequestClose={closeModal}
+          contentLabel="Example Modal"
+          className={modalCard}
+        >
+          <h2>Hello</h2>
+          <button onClick={closeModal}>close</button>
+          <div>I am a modal</div>
+        </Modal>
+      </div>
     </>
   );
 }
