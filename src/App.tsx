b6a68b46543b6c6484297204c9ff70953e854cb2
dethMastery <me@suphakit.net>
2023-04-28 12:07:56 +0700

    feat: centerize middle

--- src/App.tsx
+++ src/App.tsx
@@ -17,12 +17,17 @@ function App() {
     setIsOpen(false);
   }
 
-  const modalCard = 'absolute translate-x-[-50%] translate-y-[-50%] right-[50%] left-[50%] top-[20%] w-[80%] h-[30%] bg-[#2e2f2f] text-[whitesmoke] rounded-[1rem] p-4 turnOn'
+  const modalCard =
+    "absolute translate-x-[-50%] translate-y-[-50%] right-[50%] left-[50%] top-[20%] w-[80%] h-[30%] bg-[#2e2f2f] text-[whitesmoke] rounded-[1rem] p-4 turnOn";
 
   return (
     <>
       <div>
-        <button onClick={openModal}>Open Modal</button>
+        <div className="w-full h-screen flex flex-column justify-center text-center items-center">
+          <div>
+            <button onClick={openModal} className="p-4 bg-[#2e2f2f] text-[whitesmoke] rounded-[1rem]">Open Modal</button>
+          </div>
+        </div>
         <Modal
           isOpen={modalIsOpen}
           onAfterOpen={afterOpenModal}
@@ -30,9 +35,20 @@ function App() {
           contentLabel="Example Modal"
           className={modalCard}
         >
-          <h2>Hello</h2>
-          <button onClick={closeModal}>close</button>
-          <div>I am a modal</div>
+          <div className="flex flex-row">
+            <div className="w-full">
+              <h2 className="text-2xl w-full p-[1rem]">Hello, Modal</h2>
+            </div>
+            <div className="w-full text-right pl-4">
+              <button
+                onClick={closeModal}
+                className="bg-[#fe6e61] text-[whitesmoke] p-4 text-l rounded-[10px] duration-300 transition-all hover:text-sm"
+              >
+                X
+              </button>
+            </div>
+          </div>
+          <div className="pl-[1rem]">I am a modal</div>
         </Modal>
       </div>
     </>
